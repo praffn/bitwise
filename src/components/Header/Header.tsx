8cffb2c8c769e1b603bde4a490ba2c24c7cdbda6
Phillip Raffnsoe <phillip@praffn.dk>
2019-03-04 12:20:20 +0100

    add some a11y and seo stuff

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC<{}> = ({ children }) => {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img src={wizard} className={styles.logoImage} />
+        <img src={wizard} className={styles.logoImage} alt="Bitwizard" />
         <h1 className={styles.logoTitle}>bitwise</h1>
       </div>
       <div className={styles.rest}>{children}</div>
